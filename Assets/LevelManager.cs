6456306754b48c1c3005a829eb897e2be03086e7
Justin Salanga <adonix56@gmail.com>
2016-10-04 18:48:39 -0700

    Initial Commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	//Load a new level
+	public void LoadLevel(string name){
+		Debug.Log ("Fuckin' " + name);
+		Application.LoadLevel (name);
+	}
+
+	//Quit the game
+	public void QuitRequest(){
+		Debug.Log ("GET THE FUCK OUT THEN!");
+		//Only use on Consoles and Computers!
+		//Not on phones,web, or debug!!
+		Application.Quit ();
+	}
+
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	int min, max, guess;
+	int maxGuessesAllowed = 10;
+	public Text guessNum;
+
+	// Use this for initialization
+	void Start () {
+		StartGame();
+	}
+
+	void StartGame(){
+		min = 1;
+		max = 1000;
+		guess = 500;
+		max = max + 1;
+	}
+
+	void NextGuess() {
+		guess = (max + min) / 2;
+		guessNum.text = guess.ToString ();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+
+	public void GuessLower(){
+		max = guess;
+		NextGuess ();
+	}
+
+	public void GuessHigher(){
+		min = guess;
+		NextGuess ();
+	}
+}
